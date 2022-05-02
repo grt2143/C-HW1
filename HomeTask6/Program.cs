40a6e17dd778a4a656d11f275982d1c8a2eee5cd
Alex <alexchurilov@mail.ru>
2022-05-02 14:16:17 +0300

    Add project HomeTask6

---
+++ HomeTask6/Program.cs
@@ -0,0 +1,6 @@
+﻿// На вход подается число и выдает является ли оно чётным
+
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(n % 2 == 0 ? "Да, чётное" : "Нет, нечетное");
+
