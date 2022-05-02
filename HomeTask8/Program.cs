ce26c1eed15d5e12363119a10a0883c4cacc7751
Alex <alexchurilov@mail.ru>
2022-05-02 16:14:03 +0300

    Add project HomeTask8

---
+++ HomeTask8/Program.cs
@@ -0,0 +1,14 @@
+﻿// На вход подается число N
+// На выходе показать все чётные от 1 до N
+
+Console.WriteLine("Введите натуральное число");
+int a = 1;
+int b = int.Parse(Console.ReadLine());
+for (int i = a; i <= b; i++)
+{
+if (i % 2 == 0) 
+{
+Console.Write(i + " ");
+}
+}
+
