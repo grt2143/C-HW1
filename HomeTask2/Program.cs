ab6f0584f5f2e26e6a5971927ba4fc5ce665f653
Alex <alexchurilov@mail.ru>
2022-05-02 13:48:46 +0300

    Add project HomeTask2

---
+++ HomeTask2/Program.cs
@@ -0,0 +1,11 @@
+﻿// На вход принимается 2 числа 
+// на выход максимальное из них
+
+Console.WriteLine("Введите первое число");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int n2 = int.Parse(Console.ReadLine());
+if (n1 > n2)
+Console.WriteLine(n1+"-Большее число");
+else 
+Console.WriteLine(n2+"-Большее число");
